9bc154e7c54ade617f635d39550449b73ccf9b87
Calin Pupaza <cpupaza@adobe.com>
2024-01-15 22:20:52 +0200

    Initial commit with pyton setup and Jupyter notebook

---
+++ activate.sh
@@ -0,0 +1,1 @@
+source jirabotenv/bin/activate
---
+++ setup.sh
@@ -0,0 +1,11 @@
+python3 --version
+pip3 --version
+
+python3 -m venv jirabotenv
+source activate.sh
+
+ pip install langchain "langserve[all]" langchain-cli langsmith langchain_openai
+ pip install notebook pip install python-dotenv
+
+ pip install atlassian-python-api simplejson
+ 
