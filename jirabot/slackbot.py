3cb7def1d011d43b4ee1510768f4279ce3215bea
Calin Pupaza <cpupaza@adobe.com>
2024-01-17 10:48:14 +0200

    Slack bot config and ACK response

--- jirabot/slackbot.py
+++ jirabot/slackbot.py
@@ -11,7 +11,7 @@ slack_client = WebClient(token=slack_token)
 
 # Initialize Slack event adapter
 slack_signing_secret = os.environ.get("SLACK_SIGNING_SECRET")
-slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/slack/events")
+slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/action")
 
 # Define event handler for message events
 @slack_events_adapter.on("message")
@@ -20,8 +20,10 @@ def handle_message(event_data):
     channel_id = message["channel"]
     user_id = message.get("user")
     text = message.get("text")
-
-    # Add your bot's logic here
+    bot_id = message.get("bot_id")
+    if bot_id is None:
+        # message from a user
+        slack_client.chat_postMessage(channel=channel_id, text="ACK: " + text)
 
 # Start the event listener
 slack_events_adapter.start(port=3000)
